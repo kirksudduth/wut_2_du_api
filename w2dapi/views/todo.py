b8abd75deaabf8da0b3775d4519169da27d908d9
Kirk Sudduth <kirksudduth@gmail.com>
2020-11-29 13:48:00 -0600

    wrote the serializer code in the views/todo.py file

--- w2dapi/views/todo.py
+++ w2dapi/views/todo.py
@@ -3,3 +3,16 @@ from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import serializers
 from frest_framework import status
+
+from w2dapi.models import ToDo
+
+
+class ToDoSerializer(serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+        model = ToDo
+        url = serializers.HyperlinkedIdentityField(
+            view_name='todo',
+            lookup_field='id'
+        )
+        fields = ('id', 'doer', 'wut', 'timestamp', 'completed', 'image')
